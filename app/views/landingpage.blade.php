c1cda10261e6e87bb117e2edead63887a978922f
Jbns89 <Jbns89@gmail.com>
2014-10-18 14:15:05 -0500

    basic landing page for our site

---
+++ app/views/landingpage.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('header')
+
+<title>Landing Page</title>
+
+@stop
+
+@section('content')
+
+    <h1>This will be our awesome rissa-fied landing page</h1>
+    
+@stop
