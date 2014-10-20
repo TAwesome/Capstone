d798457588b92989b90c56db47a8e067d2d34502
Jacob Ernst <akumadude.je@gmail.com>
2014-10-20 14:47:16 -0500

    Fixed syntax.

--- app/models/Post.php
+++ app/models/Post.php
@@ -2,18 +2,14 @@
 
 class Post extends BaseModel {
     
-    public static $rules = array(
-        'content' => 'required|max:200'
-    );
-    
      public function users()
     {
         return $this->belongsTo('User');
     }
 	// Add your validation rules here
-	public static $rules = [
-		// 'title' => 'required'
-	];
+	public static $rules = array(
+        'content' => 'required|max:200'
+    );
 
 	// Don't forget to fill this array
 
