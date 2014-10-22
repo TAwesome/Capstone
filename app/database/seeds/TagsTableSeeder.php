db1aba192dd85c36f9d3ce347c0a95676b805421
Jacob Ernst <akumadude.je@gmail.com>
2014-10-22 14:53:16 -0500

    Added to the random array for the Tags seeder.

--- app/database/seeds/TagsTableSeeder.php
+++ app/database/seeds/TagsTableSeeder.php
@@ -25,12 +25,30 @@ class TagsTableSeeder extends Seeder {
             'amazing',
             'hilarious',
             'brilliant',
-            'creepy'
+            'creepy',
+            'charming',
+            'cold',
+            'creative',
+            'considerate',
+            'classic',
+            'dangerous',
+            'different',
+            'difficult',
+            'disgusting',
+            'dirty',
+            'easy',
+            'exotic',
+            'exhausted',
+            'fake',
+            'flashy',
+            'fluffy',
+            'gregarious',
+            'hideous'
         ];
         DB::table('tags')->delete();
 		$faker = Faker::create();
 
-		foreach(range(1, 15) as $index)
+		foreach(range(1, 10) as $index)
 		{
 			Tag::create([
                 'tag' => $faker->unique()->randomElement($adjectives)
