7e96326c9774764f9a978662c7652ded843dd75d
Jacob Ernst <akumadude.je@gmail.com>
2014-10-24 11:19:46 -0500

    Fixed syntax in the followers and following views.

--- app/views/following.blade.php
+++ app/views/following.blade.php
@@ -4,6 +4,6 @@
     <!-- Need to add follow/unfollow button 
     that dynamically loads based on the user's relationship to other users -->
     @foreach($data as $person)
-        <h1>{{$person}}</h1>
+        <h1>{{$person['name']}}</h1>
     @endforeach
 @stop
