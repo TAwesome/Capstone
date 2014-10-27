146cdb24604d8714216964b456fd28b4abab19ff
Jacob Ernst <akumadude.je@gmail.com>
2014-10-27 13:22:42 -0500

    moved the following JS to an external Javascript file.

--- app/views/following.blade.php
+++ app/views/following.blade.php
@@ -26,22 +26,8 @@
         </div>
     </div>
     
+        <script src="/js/following.js"></script>
         <script type="text/javascript">
-            $(".follow").click(function(event) {
-                event.preventDefault();
-                
-                var $followButton = $(this);
-                var url = $followButton.attr('href');
-                // $followButton.hide();
-                // traverser to find follow button
-                // $.get();
-                $.get(url, function(){
-                    $followButton.toggleClass('hide');
-                    $followButton.siblings('.follow').toggleClass('hide');
-                });
-                
-            });
-            
             $('#tags').tagsInput({
                     "width": "75%",
                     "height": "70px",
