6e1233f0f3a0ce7b3bbdd42f6287686b11423279
Jbns89 <Jbns89@gmail.com>
2014-10-27 14:30:55 -0500

    Added routes so all links are working, all views are extending the master layout, also changed all views to blade syntax

--- app/views/landingpage.blade.php
+++ app/views/landingpage.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('header')
-  <title>MyLanguage</title>
+  <title>SkyLanguage</title>
 @stop
 
 
@@ -216,14 +216,4 @@
 
 
 
-@section('bottom-script')
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
-    <script src="../../assets/js/docs.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-@stop
