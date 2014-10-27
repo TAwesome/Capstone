928a162ce5fb36de70eed8fff9cbdc1ef515b25c
Rissa Waters <rissa@rissawaters.com>
2014-10-27 15:57:38 -0500

    Last remnant of merge conflicts

--- app/views/TAhome.blade.php
+++ app/views/TAhome.blade.php
@@ -1,124 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-    <link rel="stylesheet" type="text/css" href="/TAprofile.css">
+@extends('layouts.master')
 
-    <!-- Custom styles for this template -->
-    <link href="cover.css" rel="stylesheet">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+@section('header')
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<title>Home</title>
 
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+@stop
 
-    <!-- Bootstrap core CSS -->
-    <link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
+@section('content')
 
-    <!-- Bootstrap core CSS -->
-    <link href="/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Bootstrap theme -->
-    <link href="/bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
+    <div class="container theme-showcase" role="main">
 
-    <!-- Custom styles for this template -->
-    <link href="theme.css" rel="stylesheet">
+      <div class="row">
+        <div class="col-sm-offset-2 col-lg-8">
+          <div class="input-group">
+            <span class="input-group-btn">
+              <button class="btn btn-default" type="button">Search</button>
+            </span>
+            <input type="text" class="form-control">
+          </div><!-- /input-group -->
+        </div><!-- /.col-lg-6 -->
+      </div><!-- /.row -->
 
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="/bootstrap/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="/bootstrap/assets/js/ie-emulation-modes-warning.js"></script>
 
-    <title>Cover Template for Bootstrap</title>
- 
-  </head>
-
-  <body role="document">
-
-    <!-- Fixed navbar -->
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">MyLanguage</a>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#">Profile</a></li>
-            <li><a href="#">Messages <span class="badge">3</span></a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact Us</a></li>
-            <li class="dropdown-nav">
-              <a href="#" id="dropdown-nav" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-
-<!--           <form class="form-inline" role="form">
-            <div class="sign-in" style="margin-left: 670px; width: 500px; margin-top: 7px;">
-              <div class="form-group">
-                <div class="input-group">
-                  <div class="input-group-addon">@</div>
-                  <input class="form-control" type="email" placeholder="Enter email">
-                </div>
-              </div>
-              <div class="form-group">
-                <label class="sr-only" for="exampleInputPassword2">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
-              </div>
-              <button type="submit" class="btn btn-default">Sign in</button>
-            </div>
-          </form> -->
-
-        </div><!--/.nav-collapse -->
+          <!-- User Posting ability -->
+      <div class="guidebar-home list-group col-sm-3">
+        <a href="#" class="list-group-item active">Home</a>
+        <a href="#" class="list-group-item">English</a>
+        <a href="#" class="list-group-item">French</a>
+        <a href="#" class="list-group-item">Spanish</a>
       </div>
+
+
+      <div class="posts-container-home col-sm-8">
+         <div>
+            <input type="text" class="form-control" placeholder="Try a new Language!">
+        </div>
+      </div>
+
     </div>
 
 
-    <div class="row">
-      <div class="col-lg-6">
-        <div class="input-group">
-          <span class="input-group-btn">
-            <button class="btn btn-default" type="button">Go!</button>
-          </span>
-          <input type="text" class="form-control">
-        </div><!-- /input-group -->
-      </div><!-- /.col-lg-6 -->
-    </div><!-- /.row -->
+@stop
+
+    
 
 
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="/bootstrap/assets/js/docs.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="/bootstrap/assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
