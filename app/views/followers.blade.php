b0d7432a52e103917436741266e0f836d8b1cb05
Jacob Ernst <akumadude.je@gmail.com>
2014-10-28 13:43:21 -0500

    Fixed the followers view to practice with likes and unlikes

--- app/views/followers.blade.php
+++ app/views/followers.blade.php
@@ -17,6 +17,29 @@
     @empty
         <h1>No Followers</h1>
     @endforelse
+    
+    <div class='container'>
+        <h1>Post 1</h1>
+        @if(Auth::user()->likes->contains(1))
+            <a href="/unlike/1" class="btn btn-danger follow">unlike</a>
+            <a href="/like/1" class="btn btn-info follow hide">like</a>
+        @else
+            <a href="/unlike/1" class="btn btn-danger follow hide">unlike</a>
+            <a href="/like/1" class="btn btn-info follow">like</a>
+        @endif
+    </div>
+    
+    <div class='container'>
+        <h1>Post 2</h1>
+        @if(Auth::user()->likes->contains(2))
+            <a href="/unlike/2" class="btn btn-danger follow">unlike</a>
+            <a href="/like/2" class="btn btn-info follow hide">like</a>
+        @else
+            <a href="/unlike/2" class="btn btn-danger follow hide">unlike</a>
+            <a href="/like/2" class="btn btn-info follow">like</a>
+        @endif
+    </div>
+    
     {{ $data->links() }}
     <script src="/js/following.js"></script>
 @stop
