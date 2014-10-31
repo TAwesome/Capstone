7853f643cf24fd78eb0705702706620c801a0af2
Jacob Ernst <akumadude.je@gmail.com>
2014-10-31 14:24:00 -0500

    Fixed following JS

--- public/js/following.js
+++ public/js/following.js
@@ -10,25 +10,3 @@ $(".follow").click(function(event) {
     });
     
 });
-
-
-// $(".translate").click(function(event) {
-//     event.preventDefault();
-    
-//     var key = '';
-    
-//     var $translateButton = $(this);
-    
-//     var text = $(.post).html();
-    
-//     var url = 'https://www.googleapis.com/language/translate/v2/translate?key=' . key;
-    
-//     // $followButton.hide();
-//     // traverser to find follow button
-//     // $.get();
-//     $.get(url, function(){
-//         $followButton.toggleClass('hide');
-//         $followButton.siblings('.follow').toggleClass('hide');
-//     });
-    
-// });
